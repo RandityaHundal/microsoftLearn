04d34814083e4af11aeff9785089e6e1898101e9
Randitya Hundal <67023582+RandityaHundal@users.noreply.github.com>
2024-01-19 17:45:29 -0800

    Create program.cs

---
+++ variable scope/program.cs
@@ -0,0 +1,25 @@
+// original code with errors
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (int number in numbers)
+{
+    int total;
+
+    total += number;
+
+    if (number == 42)
+    {
+       bool found = true;
+
+    }
+
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");
