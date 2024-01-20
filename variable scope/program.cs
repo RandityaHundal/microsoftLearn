51de45230f14ea9d820df889f71e916544a43d29
Randitya Hundal <67023582+RandityaHundal@users.noreply.github.com>
2024-01-19 17:46:03 -0800

    Update program.cs

--- variable scope/program.cs
+++ variable scope/program.cs
@@ -1,25 +1,28 @@
-// original code with errors
+// my solution to challenge with correct variable scoping
 
 int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
 
 foreach (int number in numbers)
 {
-    int total;
+    
 
     total += number;
 
     if (number == 42)
     {
-       bool found = true;
+        found = true;
 
     }
 
 }
 
-if (found) 
+if (found)  
 {
     Console.WriteLine("Set contains 42");
 
 }
+
 
 Console.WriteLine($"Total: {total}");
